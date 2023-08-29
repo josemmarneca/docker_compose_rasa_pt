f57ca4c6ee0baa998c20aee291e01c3d8ad50e1d
Ubuntu <ubuntu@ip-172-31-16-87.eu-west-3.compute.internal>
2023-08-29 11:40:45 +0000

    added custom lib

---
+++ rasa/addons/customlib/customException.py
@@ -0,0 +1,18 @@
+class CustomException(Exception):
+    pass
+
+
+class CustomConnectionException(Exception):
+    pass
+
+
+class CustomAuthenticationException(Exception):
+    pass
+
+
+class CustomMissingParamException(Exception):
+    pass
+
+
+class CustomUnsuportedIntegrationType(Exception):
+    pass
